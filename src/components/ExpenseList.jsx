0750a2e54b05212cf40c25b7e30200f5a0ac3d27
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2022-07-20 12:41:54 +0530

    changed expense list height responsive

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -3,7 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 
 function ExpenseList({ expenses, loading }) {
   return (
-    <div className="flex flex-col bg-white drop-shadow-md p-6 rounded-md h-[320px]">
+    <div className="flex flex-col bg-white drop-shadow-md p-6 rounded-md md:h-[320px] max-h-[320px]">
       <h1 className="text-2xl font-bold">Expenses</h1>
       <div className="flex flex-col overflow-y-scroll h-5/6 scrollbar-thin scrollbar-thumb-indigo-600 ">
         {loading ? (
