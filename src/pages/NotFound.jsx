26432027bf972258055c9d25d714755b677e813f
Vaibhav Tekkalur <59783171+vaibhavTekk@users.noreply.github.com>
2022-07-20 12:57:07 +0530

    added netlify config and setup 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
     <div className="flex flex-col mx-auto my-auto justify-evenly items-center">
-      <h1 className="font-bold text-3xl">Oops !</h1>
-      <h1 className="font-bold text-9xl">404</h1>
-      <h1 className="font-semibold text-3xl">Page Not Found</h1>
+      <h1 className="font-bold md:text-3xl text-2xl">Oops !</h1>
+      <h1 className="font-bold md:text-9xl text-7xl">404</h1>
+      <h1 className="font-semibold md:text-3xl text-xl">Page Not Found</h1>
+      <Link to="/" className="btn btn-primary w-1/2 drop-shadow-sm m-6">
+        Go Back
+      </Link>
     </div>
   );
 }
